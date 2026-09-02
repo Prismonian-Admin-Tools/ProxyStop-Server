51bbc2e1d7a5e716fa10f0c55ad228d5b46ac3f8
Adrian Ray LeDeaux <138169736+TheW0NK@users.noreply.github.com>
2026-09-02 19:08:18 +0000

    Changes to installer.cmd

--- installer.cmd
+++ installer.cmd
@@ -1,23 +1,8 @@
 @echo off
 setlocal
 
-echo ----- ProxyStop Server Setup Wizard -----
-echo.
-echo Select installation type:
-echo [1] Guided Installation (recommended)
-echo [2] I already have an "installer.json" file
-echo [3] Quit the installer
-choice /C 123 /N /M "Enter your choice: "
+echo "Installing ProxyStop Server..."
 
-if errorlevel 3 goto :quit
-if errorlevel 2 set "INSTALL_TYPE=config"
-if errorlevel 1 if not defined INSTALL_TYPE set "INSTALL_TYPE=guided"
+mkdir fingerprints
+npm install
 
-echo.
-echo Selected installation type: %INSTALL_TYPE%
-goto :eof
-
-:quit
-echo.
-echo Exiting the installer.
-endlocal
